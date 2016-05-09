8a6d9e78529970b753d29cc839f23baaf2f2edf5
David Michael Zalk <dzknj@outlook.com>
2016-05-08 19:18:33 -0700

    added .eslintrc file to make linter work properly

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const Hapi = require('hapi');
-const Animal = require(__dirname + '/models/animals');
-const animalRoutes = require(__dirname + '/routes/animalroutes');
-const serverError = require(__dirname + '/lib/servererror');
+const Animal = require(__dirname + '/models/animals'); // eslint-disable-line
+const animalRoutes = require(__dirname + '/routes/animalroutes'); // eslint-disable-line
+const serverError = require(__dirname + '/lib/servererror'); // eslint-disable-line
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/db');
